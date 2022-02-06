51d24c147be1224bacb2ac5c04625f0a5b344da1
Marianne Rachid <marianne.rachid@gmail.com>
2022-02-06 14:11:08 +0000

    Add Integration Tests Part 2
    Tested:
     - Create with `--file` args
     - Create with `--public-key-path` args
     - Get with `--file` args
     - Delete with `--file` args
     - List with `--namespace` args

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -1,7 +1,10 @@
 package integration_test
 
 import (
+	b64 "encoding/base64"
 	"encoding/json"
+	"io/ioutil"
+	"os"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -23,8 +26,7 @@ var _ = Describe("Integration", func() {
 
 		AfterEach(func() {
 			cmd := command{action: "delete", args: []string{"--id", *result.Microvm.Spec.Uid}}
-			deleteSession := executeCommand(cmd)
-			Eventually(deleteSession).Should(gexec.Exit(0))
+			Eventually(executeCommand(cmd)).Should(gexec.Exit(0))
 		})
 
 		It("can create a default microVM", func() {
@@ -46,6 +48,48 @@ var _ = Describe("Integration", func() {
 			Expect(result.Microvm.Spec.Id).To(Equal(name))
 			Expect(result.Microvm.Spec.Namespace).To(Equal(namespace))
 		})
+
+		It("can create a microVM from a file", func() {
+			cmd := command{action: "create", args: []string{"--file", "./../../example.json", "--name", "this will be overriden"}}
+			session := executeCommand(cmd)
+			Eventually(session).Should(gexec.Exit(0))
+
+			Expect(json.Unmarshal(session.Wait().Out.Contents(), &result)).To(Succeed())
+			Expect(result.Microvm.Spec.Id).To(Equal("mvm1"))
+			Expect(result.Microvm.Spec.Namespace).To(Equal("ns1"))
+		})
+
+		Context("when passing ssh key file path", func() {
+			var (
+				keyFile *os.File
+				key     = "this is a test key woohoo"
+			)
+
+			BeforeEach(func() {
+				var err error
+				keyFile, err = ioutil.TempFile("", "ssh_key")
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ioutil.WriteFile(keyFile.Name(), []byte(key), 0)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			AfterEach(func() {
+				Expect(os.Remove(keyFile.Name())).To(Succeed())
+			})
+
+			It("can create a microVM with a ssh key", func() {
+				cmd := command{action: "create", args: []string{"--public-key-path", keyFile.Name()}}
+				session := executeCommand(cmd)
+				Eventually(session).Should(gexec.Exit(0))
+
+				Expect(json.Unmarshal(session.Wait().Out.Contents(), &result)).To(Succeed())
+				userData, err := b64.StdEncoding.DecodeString(result.Microvm.Spec.Metadata["user-data"])
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(userData)).To(ContainSubstring(key))
+			})
+		})
+
 	})
 
 	Context("Get", func() {
@@ -60,8 +104,7 @@ var _ = Describe("Integration", func() {
 
 		AfterEach(func() {
 			cmd := command{action: "delete", args: []string{"--id", *result.Microvm.Spec.Uid}}
-			deleteSession := executeCommand(cmd)
-			Eventually(deleteSession).Should(gexec.Exit(0))
+			Eventually(executeCommand(cmd)).Should(gexec.Exit(0))
 		})
 
 		It("gets MicroVm", func() {
@@ -81,6 +124,36 @@ var _ = Describe("Integration", func() {
 			Eventually(session).Should(gexec.Exit(0))
 			Eventually(session.Out, "30s").Should(gbytes.Say("CREATED"))
 		})
+
+		Context("when passing a json file", func() {
+			var getFile *os.File
+
+			BeforeEach(func() {
+				var err error
+				content, err := json.Marshal(result.Microvm.Spec)
+				Expect(err).NotTo(HaveOccurred())
+				getFile, err = ioutil.TempFile("", "tempfile")
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ioutil.WriteFile(getFile.Name(), []byte(content), 0)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			AfterEach(func() {
+				Expect(os.Remove(getFile.Name())).To(Succeed())
+			})
+
+			It("gets MicroVm", func() {
+				cmd := command{action: "get", args: []string{"--id", "this will be overriden", "--file", getFile.Name()}}
+				session := executeCommand(cmd)
+				Eventually(session).Should(gexec.Exit(0))
+
+				var getResult v1alpha1.GetMicroVMResponse
+
+				Expect(json.Unmarshal(session.Wait().Out.Contents(), &getResult)).To(Succeed())
+				Expect(getResult.Microvm.Spec.Id).To(Equal(defaultName))
+			})
+		})
 	})
 
 	Context("Delete", func() {
@@ -95,29 +168,57 @@ var _ = Describe("Integration", func() {
 
 		It("deletes MicroVm", func() {
 			cmd := command{action: "delete", args: []string{"--id", *result.Microvm.Spec.Uid}}
-			deleteSession := executeCommand(cmd)
-			Eventually(deleteSession).Should(gexec.Exit(0))
+			Eventually(executeCommand(cmd)).Should(gexec.Exit(0))
 
 			getCmd := command{action: "get", args: []string{"--id", *result.Microvm.Spec.Uid}}
 			getSession := executeCommand(getCmd)
 			Eventually(getSession).Should(gexec.Exit(1))
 			Eventually(getSession.Err).Should(gbytes.Say("OHH WHAT A DISASTER"))
 		})
+
+		Context("when passing a json file", func() {
+			var deleteFile *os.File
+
+			BeforeEach(func() {
+				var err error
+				content, err := json.Marshal(result.Microvm.Spec)
+				Expect(err).NotTo(HaveOccurred())
+				deleteFile, err = ioutil.TempFile("", "tempfile")
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ioutil.WriteFile(deleteFile.Name(), []byte(content), 0)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			AfterEach(func() {
+				Expect(os.Remove(deleteFile.Name())).To(Succeed())
+			})
+
+			It("deletes MicroVm", func() {
+				cmd := command{action: "delete", args: []string{"--id", "this will be overriden", "--file", deleteFile.Name()}}
+				Eventually(executeCommand(cmd)).Should(gexec.Exit(0))
+
+				getCmd := command{action: "get", args: []string{"--id", *result.Microvm.Spec.Uid}}
+				getSession := executeCommand(getCmd)
+				Eventually(getSession).Should(gexec.Exit(1))
+				Eventually(getSession.Err).Should(gbytes.Say("OHH WHAT A DISASTER"))
+			})
+		})
 	})
 
 	Context("List", func() {
 		Context("when microVMs exist", func() {
-			var result v1alpha1.CreateMicroVMResponse
+			var result1 v1alpha1.CreateMicroVMResponse
 
 			BeforeEach(func() {
 				createCmd := command{action: "create"}
 				session := executeCommand(createCmd)
 				Eventually(session).Should(gexec.Exit(0))
-				Expect(json.Unmarshal(session.Wait().Out.Contents(), &result)).To(Succeed())
+				Expect(json.Unmarshal(session.Wait().Out.Contents(), &result1)).To(Succeed())
 			})
 
 			AfterEach(func() {
-				cmd := command{action: "delete", args: []string{"--id", *result.Microvm.Spec.Uid}}
+				cmd := command{action: "delete", args: []string{"--id", *result1.Microvm.Spec.Uid}}
 				deleteSession := executeCommand(cmd)
 				Eventually(deleteSession).Should(gexec.Exit(0))
 			})
@@ -129,7 +230,34 @@ var _ = Describe("Integration", func() {
 
 				var list v1alpha1.ListMicroVMsResponse
 				Expect(json.Unmarshal(session.Wait().Out.Contents(), &list)).To(Succeed())
-				Expect(list.Microvm[0].Spec.Uid).To(Equal(result.Microvm.Spec.Uid))
+				Expect(list.Microvm[0].Spec.Uid).To(Equal(result1.Microvm.Spec.Uid))
+			})
+
+			Context("when passing namespace as args", func() {
+				var result2 v1alpha1.CreateMicroVMResponse
+
+				BeforeEach(func() {
+					createCmd := command{action: "create", args: []string{"--namespace", namespace}}
+					session := executeCommand(createCmd)
+					Eventually(session).Should(gexec.Exit(0))
+					Expect(json.Unmarshal(session.Wait().Out.Contents(), &result2)).To(Succeed())
+				})
+
+				AfterEach(func() {
+					cmd := command{action: "delete", args: []string{"--id", *result2.Microvm.Spec.Uid}}
+					Eventually(executeCommand(cmd)).Should(gexec.Exit(0))
+				})
+
+				It("lists MicroVm", func() {
+					cmd := command{action: "list", args: []string{"--namespace", namespace}}
+					session := executeCommand(cmd)
+					Eventually(session).Should(gexec.Exit(0))
+
+					var list v1alpha1.ListMicroVMsResponse
+					Expect(json.Unmarshal(session.Wait().Out.Contents(), &list)).To(Succeed())
+					Expect(list.Microvm[0].Spec.Uid).To(Equal(result2.Microvm.Spec.Uid))
+				})
+
 			})
 		})
 
