411c3f3a52f834e70e2ac82144e727ed9d10b8ad
Marianne Rachid <marianne.rachid@gmail.com>
2022-03-23 15:22:20 +0000

    Extract Create client to pkg/client and add unit tests
    Changes:
      - Extracted Create client to pkg/client
      - Added unit test for Create Client
      - Extracted `isSet` function to pkg/utils

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,14 +10,12 @@ import (
 	"os"
 	"strings"
 
+	"github.com/Callisto13/hammertime/pkg/client"
 	"github.com/Callisto13/hammertime/pkg/utils"
 	"github.com/urfave/cli/v2"
-	"gopkg.in/yaml.v2"
 
 	"github.com/weaveworks/flintlock/api/services/microvm/v1alpha1"
 	"github.com/weaveworks/flintlock/api/types"
-	"github.com/weaveworks/flintlock/client/cloudinit/instance"
-	"github.com/weaveworks/flintlock/client/cloudinit/userdata"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -101,7 +98,9 @@ func main() {
 					}
 					defer conn.Close()
 
-					res, err := createMicrovm(v1alpha1.NewMicroVMClient(conn), mvmName, mvmNamespace, sshKeyPath, jsonSpec)
+					client := client.New(v1alpha1.NewMicroVMClient(conn))
+
+					res, err := client.Create(mvmName, mvmNamespace, jsonSpec, sshKeyPath)
 					if err != nil {
 						return err
 					}
@@ -354,35 +353,6 @@ func isSet(flag string) bool {
 	return flag != ""
 }
 
-func createMicrovm(client v1alpha1.MicroVMClient, name, ns, sshPath, jsonSpec string) (*v1alpha1.CreateMicroVMResponse, error) {
-	var (
-		mvm *types.MicroVMSpec
-		err error
-	)
-
-	if jsonSpec != "" {
-		mvm, err = loadSpecFromFile(jsonSpec)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		mvm, err = defaultMicroVM(name, ns, sshPath)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	createReq := v1alpha1.CreateMicroVMRequest{
-		Microvm: mvm,
-	}
-	resp, err := client.CreateMicroVM(context.Background(), &createReq)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
-}
-
 func getMicrovm(client v1alpha1.MicroVMClient, uid string) (*v1alpha1.GetMicroVMResponse, error) {
 	getReq := v1alpha1.GetMicroVMRequest{
 		Uid: uid,
@@ -418,113 +388,6 @@ func listMicrovms(client v1alpha1.MicroVMClient, name, ns string) (*v1alpha1.Lis
 	}
 
 	return resp, nil
-}
-
-func defaultMicroVM(name, namespace, sshPath string) (*types.MicroVMSpec, error) {
-	var (
-		kernelImage = "ghcr.io/weaveworks/flintlock-kernel:5.10.77"
-		cloudImage  = "ghcr.io/weaveworks/capmvm-kubernetes:1.21.8"
-	)
-
-	metaData, err := createMetadata(name, namespace)
-	if err != nil {
-		return nil, err
-	}
-	userData, err := createUserData(name, sshPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return &types.MicroVMSpec{
-		Id:         name,
-		Namespace:  namespace,
-		Vcpu:       2,
-		MemoryInMb: 2048,
-		Kernel: &types.Kernel{
-			Image:            kernelImage,
-			Filename:         utils.PointyString("boot/vmlinux"),
-			AddNetworkConfig: true,
-		},
-		RootVolume: &types.Volume{
-			Id:         "root",
-			IsReadOnly: false,
-			MountPoint: "/",
-			Source: &types.VolumeSource{
-				ContainerSource: utils.PointyString(cloudImage),
-			},
-		},
-		Interfaces: []*types.NetworkInterface{
-			{
-				DeviceId: "eth1",
-				Type:     0,
-			},
-		},
-		Metadata: map[string]string{
-			"meta-data": metaData,
-			"user-data": userData,
-		},
-	}, nil
-}
-
-func createUserData(name, sshPath string) (string, error) {
-	defaultUser := userdata.User{
-		Name: "root",
-	}
-
-	if sshPath != "" {
-		sshKey, err := getKeyFromPath(sshPath)
-		if err != nil {
-			return "", err
-		}
-		defaultUser.SSHAuthorizedKeys = []string{
-			sshKey,
-		}
-	}
-
-	// TODO: remove the boot command temporary fix after image-builder #6
-	userData := &userdata.UserData{
-		HostName: name,
-		Users: []userdata.User{
-			defaultUser,
-		},
-		FinalMessage: "The Liquid Metal booted system is good to go after $UPTIME seconds",
-		BootCommands: []string{
-			"ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf",
-		},
-	}
-
-	data, err := yaml.Marshal(userData)
-	if err != nil {
-		return "", fmt.Errorf("marshalling bootstrap data: %w", err)
-	}
-
-	dataWithHeader := append([]byte("#cloud-config\n"), data...)
-
-	return base64.StdEncoding.EncodeToString(dataWithHeader), nil
-}
-
-func createMetadata(name, ns string) (string, error) {
-	metadata := instance.New(
-		instance.WithInstanceID(fmt.Sprintf("%s/%s", ns, name)),
-		instance.WithLocalHostname(name),
-		instance.WithPlatform("liquid_metal"),
-	)
-
-	userMeta, err := yaml.Marshal(metadata)
-	if err != nil {
-		return "", fmt.Errorf("unable to marshal metadata: %w", err)
-	}
-
-	return base64.StdEncoding.EncodeToString(userMeta), nil
-}
-
-func getKeyFromPath(path string) (string, error) {
-	key, err := ioutil.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-
-	return string(key), nil
 }
 
 func processFile(file string) (string, string, string, error) {
---
+++ pkg/client/client_test.go
@@ -0,0 +1,207 @@
+package client_test
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/weaveworks/flintlock/api/services/microvm/v1alpha1"
+	"github.com/weaveworks/flintlock/api/types"
+	"github.com/weaveworks/flintlock/client/cloudinit/instance"
+	"github.com/weaveworks/flintlock/client/cloudinit/userdata"
+	"gopkg.in/yaml.v2"
+
+	"github.com/Callisto13/hammertime/pkg/client"
+	"github.com/Callisto13/hammertime/pkg/client/fakeclient"
+)
+
+var _ = Describe("Client", func() {
+	var (
+		name      = "Pantalaimon"
+		namespace = "Casper"
+	)
+
+	It("creates a MicroVm", func() {
+		mockClient := new(fakeclient.FakeMicroVMClient)
+		c := client.New(mockClient)
+
+		metaData, err := yaml.Marshal(instance.New(
+			instance.WithInstanceID(fmt.Sprintf("%s/%s", namespace, name)),
+			instance.WithLocalHostname(name),
+			instance.WithPlatform("liquid_metal"),
+		))
+		Expect(err).ToNot(HaveOccurred())
+		metadata := base64.StdEncoding.EncodeToString(metaData)
+
+		userData := &userdata.UserData{
+			HostName: name,
+			Users: []userdata.User{
+				{
+					Name: "root",
+				},
+			},
+			FinalMessage: "The Liquid Metal booted system is good to go after $UPTIME seconds",
+			BootCommands: []string{
+				"ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf",
+			},
+		}
+		data, err := yaml.Marshal(userData)
+		Expect(err).ToNot(HaveOccurred())
+
+		dataWithHeader := append([]byte("#cloud-config\n"), data...)
+		userdata := base64.StdEncoding.EncodeToString(dataWithHeader)
+		microVm := &v1alpha1.CreateMicroVMResponse{
+			Microvm: &types.MicroVM{
+				Spec: &types.MicroVMSpec{
+					Id:        name,
+					Namespace: namespace,
+				},
+			},
+		}
+
+		mockClient.CreateMicroVMReturns(microVm, nil)
+		result, err := c.Create(name, namespace, "", "")
+		Expect(err).ToNot(HaveOccurred())
+
+		_, inputReq, _ := mockClient.CreateMicroVMArgsForCall(0)
+		Expect(inputReq.Microvm.Id).To(Equal(name))
+		Expect(inputReq.Microvm.Namespace).To(Equal(namespace))
+		Expect(inputReq.Microvm.Metadata["meta-data"]).To(Equal(metadata))
+		Expect(inputReq.Microvm.Metadata["user-data"]).To(Equal(userdata))
+
+		Expect(mockClient.CreateMicroVMCallCount()).To(Equal(1))
+		Expect(result.Microvm.Spec.Id).To(Equal(name))
+		Expect(result.Microvm.Spec.Namespace).To(Equal(namespace))
+	})
+
+	Context("when using sshkey", func() {
+		Context("when file exists", func() {
+			var (
+				keyFile *os.File
+				key     = "this is a test key woohoo"
+			)
+
+			BeforeEach(func() {
+				var err error
+				keyFile, err = ioutil.TempFile("", "ssh_key")
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ioutil.WriteFile(keyFile.Name(), []byte(key), 0)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			AfterEach(func() {
+				Expect(os.Remove(keyFile.Name())).To(Succeed())
+			})
+
+			It("creates a MicroVm", func() {
+				mockClient := new(fakeclient.FakeMicroVMClient)
+				c := client.New(mockClient)
+
+				metaData, err := yaml.Marshal(instance.New(
+					instance.WithInstanceID(fmt.Sprintf("%s/%s", namespace, name)),
+					instance.WithLocalHostname(name),
+					instance.WithPlatform("liquid_metal"),
+				))
+				Expect(err).ToNot(HaveOccurred())
+				metadata := base64.StdEncoding.EncodeToString(metaData)
+
+				userData := &userdata.UserData{
+					HostName: name,
+					Users: []userdata.User{
+						{
+							Name:              "root",
+							SSHAuthorizedKeys: []string{key},
+						},
+					},
+					FinalMessage: "The Liquid Metal booted system is good to go after $UPTIME seconds",
+					BootCommands: []string{
+						"ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf",
+					},
+				}
+				data, err := yaml.Marshal(userData)
+				Expect(err).ToNot(HaveOccurred())
+
+				dataWithHeader := append([]byte("#cloud-config\n"), data...)
+				userdata := base64.StdEncoding.EncodeToString(dataWithHeader)
+				microVm := &v1alpha1.CreateMicroVMResponse{
+					Microvm: &types.MicroVM{
+						Spec: &types.MicroVMSpec{
+							Id:        name,
+							Namespace: namespace,
+						},
+					},
+				}
+
+				mockClient.CreateMicroVMReturns(microVm, nil)
+				result, err := c.Create(name, namespace, "", keyFile.Name())
+				Expect(err).ToNot(HaveOccurred())
+
+				_, inputReq, _ := mockClient.CreateMicroVMArgsForCall(0)
+				Expect(inputReq.Microvm.Id).To(Equal(name))
+				Expect(inputReq.Microvm.Namespace).To(Equal(namespace))
+				Expect(inputReq.Microvm.Metadata["meta-data"]).To(Equal(metadata))
+				Expect(inputReq.Microvm.Metadata["user-data"]).To(Equal(userdata))
+
+				Expect(mockClient.CreateMicroVMCallCount()).To(Equal(1))
+				Expect(result.Microvm.Spec.Id).To(Equal(name))
+				Expect(result.Microvm.Spec.Namespace).To(Equal(namespace))
+			})
+		})
+
+		Context("when file does not exist", func() {
+			It("returns an error", func() {
+				mockClient := new(fakeclient.FakeMicroVMClient)
+				c := client.New(mockClient)
+				_, err := c.Create(name, namespace, "", "key.txt")
+				Expect(err.Error()).To(ContainSubstring("no such file or directory"))
+			})
+		})
+	})
+
+	Context("jsonSpec is set", func() {
+		var (
+			jsonSpec  = "./../../example.json"
+			name      = "mvm1"
+			namespace = "ns1"
+		)
+
+		It("creates a MicroVm", func() {
+			mockClient := new(fakeclient.FakeMicroVMClient)
+			c := client.New(mockClient)
+
+			microVm := &v1alpha1.CreateMicroVMResponse{
+				Microvm: &types.MicroVM{
+					Spec: &types.MicroVMSpec{
+						Id:        name,
+						Namespace: namespace,
+					},
+				},
+			}
+
+			mockClient.CreateMicroVMReturns(microVm, nil)
+			result, err := c.Create("", "", jsonSpec, "")
+			Expect(err).ToNot(HaveOccurred())
+
+			_, inputReq, _ := mockClient.CreateMicroVMArgsForCall(0)
+			Expect(inputReq.Microvm.Id).To(Equal(name))
+			Expect(inputReq.Microvm.Namespace).To(Equal(namespace))
+
+			Expect(mockClient.CreateMicroVMCallCount()).To(Equal(1))
+			Expect(result.Microvm.Spec.Id).To(Equal(name))
+			Expect(result.Microvm.Spec.Namespace).To(Equal(namespace))
+		})
+
+		Context("when file does not exist", func() {
+			It("returns an error", func() {
+				mockClient := new(fakeclient.FakeMicroVMClient)
+				c := client.New(mockClient)
+				_, err := c.Create("", "", "./../../example1.json", "")
+				Expect(err.Error()).To(ContainSubstring("no such file or directory"))
+			})
+		})
+	})
+})
