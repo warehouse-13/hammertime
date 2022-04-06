a41f366961084c6ba8eca797cff48b76e5bfeab6
Claudia <claudiaberesford@gmail.com>
2022-04-06 16:49:06 +0100

    chore(dep): Update tests to use Ginkgo v2 (#38)

--- test/integration/integration_suite_test.go
+++ test/integration/integration_suite_test.go
@@ -4,7 +4,7 @@ import (
 	"os/exec"
 	"testing"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
